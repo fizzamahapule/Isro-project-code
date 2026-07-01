c9a50566a4bd7533031c0934b5b2a848f9950a91
fizzamahapule <mahapulefizza@gmail.com>
2026-07-01 10:13:54 +0530

    Refactor data loading to use Excel and preprocess data
    Updated data loading functions to use Excel file and added data preprocessing steps.

--- backend/app/services/data_loader.py
+++ backend/app/services/data_loader.py
@@ -1,15 +1,17 @@
 import pandas as pd
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-def load_rainfall():
+DATA_PATH = BASE_DIR / "datasets" / "india_all_states_climate_10year_master.xlsx"
 
-    data = pd.read_csv("datasets/rainfall.csv")
+df = pd.read_excel(DATA_PATH)
 
-    return data
+# Convert Date column
+df["Date"] = pd.to_datetime(df["Date"])
 
+# Remove missing values
+df = df.fillna(0)
 
-def load_temperature():
-
-    data = pd.read_csv("datasets/temperature.csv")
-
-    return data
+print("Dataset Loaded Successfully")
+print(df.head())
