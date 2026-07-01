dbd81f5b62576c9ce5d5b91d2bca1a4a608a8ce4
fizzamahapule <mahapulefizza@gmail.com>
2026-07-01 10:15:13 +0530

    Refactor climate routes to include state and latest data
    Removed the current climate data endpoint and added endpoints to retrieve states and latest data.

--- backend/app/routes/climate.py
+++ backend/app/routes/climate.py
@@ -1,16 +1,27 @@
 from fastapi import APIRouter
 
+from app.services.data_loader import (
+    get_latest_data,
+    get_latest_state,
+    get_all_states
+)
+
 router = APIRouter(
     prefix="/climate",
     tags=["Climate"]
 )
 
-@router.get("/current")
-def current():
 
-    return {
-        "rainfall":215,
-        "temperature":32,
-        "humidity":68,
-        "prediction":"Heavy Rain"
-    }
+@router.get("/states")
+def states():
+    return get_all_states()
+
+
+@router.get("/latest")
+def latest():
+    return get_latest_data().to_dict(orient="records")
+
+
+@router.get("/{state}")
+def state(state: str):
+    return get_latest_state(state).to_dict(orient="records")
